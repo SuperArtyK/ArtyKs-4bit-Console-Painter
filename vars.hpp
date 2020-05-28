f3f5e31cbb6f7f39970f6ac59ff3c29ef6f1c2a7
Artemy Kozhemyak <artemiy.kozhemyak@gmail.com>
2020-05-28 11:59:40 -0700

    1.0.3.832 Update final

--- vars.hpp
+++ vars.hpp
@@ -44,7 +44,7 @@ short cx;
 short fontsize = 8;
 int line = 5, column = 5, x;
 int maxl = 64, maxc = 64;
-string build = "768";
+string build = "832";
 char text[1048576];
 string spicture = "";
 char picture[] = "! bbbbb  bbbbbb  bbbbbbbb bb    bb aa   aa  c   eeeeeee     dd   dd cccccc  aa 99999999!bb   bb bb   bb    bb     bb  bb  aa  aa   c   ee          dd   dd cc   cc aa    99   !bbbbbbb bbbbbb     bb      bbbb   aaaaa        eeeeeee     ddddddd cccccc  aa    99   !bb   bb bb   bb    bb       bb    aa  aa            ee          dd cc   cc aa    99   !bb   bb bb   bb    bb       bb    aa   aa      eeeeeee          dd cccccc  aa    99   !! 333333  333333  333    33 3333333  333333  33      3333333 !33      33    33 3333   33 33      33    33 33      33      !33      33    33 33 33  33 3333333 33    33 33      33333   !33      33    33 33  33 33      33 33    33 33      33      ! 333333  333333  33   3333 3333333  333333  3333333 3333333 !                                                            !222222   22222  22 222    22 22222222 2222222 222222        !22   22 22   22 22 2222   22    22    22      22   22       !222222  2222222 22 22 22  22    22    22222   222222        !22      22   22 22 22  22 22    22    22      22   22       !22      22   22 22 22   2222    22    2222222 22   22 ";
